7cf55ab944dceb659142060c6d5284e041f4e991
jobitoalv <74395645+jobitoalv@users.noreply.github.com>
2021-11-09 17:35:20 +1000

    added id to each components

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -3,6 +3,12 @@ import "./topbar.scss"
 export default function Topbar() {
     return (
         <div className="topbar">
+            <div className="wrapper">
+                <div className="left">
+                    <a href="#intro" >ohlala</a>
+                </div>
+                <div className="right"></div>
+            </div>
 
         </div>
     )
