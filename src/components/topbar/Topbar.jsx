3d0b39ba210cd248ef2f4223466af001b287cbaa
jobitoalv <74395645+jobitoalv@users.noreply.github.com>
2021-11-10 12:26:10 +1000

    edit topbar with contacts and icons

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,11 +1,18 @@
-import "./topbar.scss"
+import "./topbar.scss";
+import {Person,Mail} from "@material-ui/icons"
 
 export default function Topbar() {
     return (
         <div className="topbar">
             <div className="wrapper">
                 <div className="left">
-                    <a href="#intro" >ohlala</a>
+                    <a href="#intro" className="logo">Jobito.</a>
+                     <div className="itemContainer" >
+                        <Person className="icon" />
+                        <span> +61 434 234 214 </span>
+                        <Mail className="icon" />
+                        <span> Jobitoalv@gmail.com </span>
+                     </div>
                 </div>
                 <div className="right"></div>
             </div>
