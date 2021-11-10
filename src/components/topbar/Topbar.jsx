820e32a0e9677273379ba16a5c1978c5ae1379ee
jobitoalv <74395645+jobitoalv@users.noreply.github.com>
2021-11-10 13:17:36 +1000

    adding hamburger to the right handside of page

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -3,19 +3,27 @@ import {Person,Mail} from "@material-ui/icons"
 
 export default function Topbar() {
     return (
-        <div className="topbar">
+        <div className="topbar ">
             <div className="wrapper">
                 <div className="left">
                     <a href="#intro" className="logo">Jobito.</a>
-                     <div className="itemContainer" >
+                      <div className="itemContainer" >
                         <Person className="icon" />
                         <span> +61 434 234 214 </span>
+                      </div>
+                      <div className="itemContainer" >
                         <Mail className="icon" />
                         <span> Jobitoalv@gmail.com </span>
-                     </div>
+                      </div>
                 </div>
-                <div className="right"></div>
-            </div>
+                <div className="right">
+                    <div className="hamburger">
+                        <span className="line1"></span>
+                        <span className="line2"></span>
+                        <span className="line3"></span>
+                    </div>
+                </div>
+             </div>
 
         </div>
     )
