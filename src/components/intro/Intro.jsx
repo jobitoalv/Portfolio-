f3dcb5a66120c565e51d4e3d2226fe43baa0c7e9
jobitoalv <74395645+jobitoalv@users.noreply.github.com>
2021-11-16 10:51:51 +1000

    yarn itupe for animation effort on words

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -8,7 +8,17 @@ export default function Intro() {
                   <img src="assets/job.jpeg" alt=""/>
               </div>
             </div>
-            <div className="right"></div>
+            <div className="right">
+                <div className="wrapper">
+                    <h2>Hi There, I`m </h2>
+                    <h1>Job Alvarez Chow</h1>
+                    <h3>A <span>Full-stack Developer</span></h3>
+                </div>
+                <a href="#portfolio">
+                    <img src="assets/down.png" alt=""/>
+                </a>
+
+            </div>
         </div>
     )
 }
