e7fd8c3fcc0cf20729b2dd8407dc34da47799d84
jobitoalv <74395645+jobitoalv@users.noreply.github.com>
2021-11-16 12:55:32 +1000

    added past projects

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -3,7 +3,32 @@ import "./portfolio.scss"
 export default function Portfolio() {
     return (
         <div className="portfolio" id="portfolio">
-            
+            <h1> Portfolio </h1>
+            <ul>
+                <li className="active">Featured</li>
+                <li>Web App</li>
+                <li>Modile App</li>
+                <li>Design</li>
+                <li>Branding</li>
+            </ul>
+            <div className="container">
+                <div className="item">
+                    <img src="assets/home.png" alt="" />
+                    <h3>Dog Gromming Market Place App </h3>
+                </div>
+                <div className="item">
+                    <img src="assets/first.png" alt="" />
+                    <h3>My first ever HTML/CSS portfolio </h3>
+                </div>
+                <div className="item">
+                    <img src="assets/ruby.png" alt="" />
+                    <h3>Ruby Game The Hangman-game </h3>
+                </div>
+                <div className="item">
+                    <img src="assets/ruby2.png" alt="" />
+                    <h3>Terminal dog grooming booking Application </h3>
+                </div>
+            </div>
         </div>
     )
 }
